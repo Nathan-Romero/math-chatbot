5196c1e3c8d029a259d7b35407d274a274db8139
Nathan Romero <schmutts@yahoo.ca>
2024-10-22 18:32:49 -0400

    Update math_chatbot.py

--- math_chatbot.py
+++ math_chatbot.py
@@ -38,3 +38,40 @@ def SpeakText(command):
     engine.say(command) 
     engine.runAndWait()
 
+# Loop infinitely for user
+while True:    
+    
+    # Exception handling to handle
+    # exceptions at runtime
+    try:
+        
+        # use the microphone as source for input
+        with sr.Microphone() as source2:
+            
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level 
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+            
+            # listens for the user's input 
+            audio2 = r.listen(source2)
+
+            # use Google API to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            # print what the user said through their microphone
+            print(MyText)
+
+            # send the user's input to the Google API and the model, then grab its output
+            response = chat_session.send_message(MyText)
+            SpeakText(response.text)
+
+    except sr.RequestError as e:
+        print("Could not request results")
+
+    except sr.UnknownValueError:
+        print("An unknown error occurred")
+
+# print the response from the math chatbot
+print(response.text)
